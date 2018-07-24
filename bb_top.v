2af9a2ec10aa18911bd3952666f9e3576111cd61
Henryk Paluch <hpaluch@seznam.cz>
2018-07-24 19:43:05 +0200

    Lesson 2 - inverter

--- bb_top.v
+++ bb_top.v
@@ -3,9 +3,12 @@
 // Top Level module
 //
 module bb_top(nled,nrst);
-  output nled;
+  output [7:0] nled;
   input nrst;
 
-  assign nled = nrst;
+  assign nled[0] =  nrst;
+  assign nled[1] = ~nrst;
+  assign nled[7:2] = 6'b111111;
 
 endmodule
+
