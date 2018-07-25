bc9f9bd73287c071bb3ef4cadc5a33c44a638706
Henryk Paluch <hpaluch@seznam.cz>
2018-07-25 18:20:44 +0200

    Output 4.7Hz clock on LED D2

--- bb_top.v
+++ bb_top.v
@@ -14,8 +14,13 @@ module bb_top(nled,nrst);
 
   // now we can use positive logic (1=active, 0=inactive)
   assign led[0]   = ~rst;
-  assign led[1]   = rst;
   assign led[7:2] = 6'b000000;
+
+  wire clk; // 4.7Hz internal clock from OSCTIMER module  
+  assign led[1]   = clk; // output 4.7Hz clok on LED D2
+
+  defparam I1.TIMER_DIV = "1048576";
+  OSCTIMER I1 (.DYNOSCDIS(1'b0), .TIMERRES(1'b0), .OSCOUT(), .TIMEROUT(clk) );
 
 endmodule
 
