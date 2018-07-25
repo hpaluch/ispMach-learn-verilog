7b8e7f5129de0844d40539edc426b8c6d08b9ee8
Henryk Paluch <hpaluch@seznam.cz>
2018-07-25 19:40:51 +0200

    Fixed reverse LED D1 function (for cherry-pick to master)

--- bb_top.v
+++ bb_top.v
@@ -13,11 +13,11 @@ module bb_top(nled,nrst);
   assign nled = ~led;
 
   // now we can use positive logic (1=active, 0=inactive)
-  assign led[0]   = ~rst;
+  assign led[0]   = rst;
   assign led[7:4] = 4'h0;
 
   wire clk; // 4.7Hz internal clock from OSCTIMER module  
-  assign led[1]   = clk; // output 4.7Hz clok on LED D2
+  assign led[1]   = clk; // output 4.7Hz clock on LED D2
 
   defparam I1.TIMER_DIV = "1048576";
   OSCTIMER I1 (.DYNOSCDIS(1'b0), .TIMERRES(1'b0), .OSCOUT(), .TIMEROUT(clk) );
