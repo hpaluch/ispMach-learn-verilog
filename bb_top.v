57a6f7a7ab3721e3986a64b799b20f59580ec51d
Henryk Paluch <hpaluch@seznam.cz>
2018-07-25 18:32:47 +0200

    2-bit counter with async RESET

--- bb_top.v
+++ bb_top.v
@@ -14,13 +14,25 @@ module bb_top(nled,nrst);
 
   // now we can use positive logic (1=active, 0=inactive)
   assign led[0]   = ~rst;
-  assign led[7:2] = 6'b000000;
+  assign led[7:4] = 4'h0;
 
   wire clk; // 4.7Hz internal clock from OSCTIMER module  
   assign led[1]   = clk; // output 4.7Hz clok on LED D2
 
   defparam I1.TIMER_DIV = "1048576";
   OSCTIMER I1 (.DYNOSCDIS(1'b0), .TIMERRES(1'b0), .OSCOUT(), .TIMEROUT(clk) );
+
+  // 2-bit counter (divider by 4)
+  reg    [1:0] counter;
+  assign led[3:2] = counter;
+
+  always @(posedge clk or posedge rst)
+  begin
+    if (rst)
+      counter <= 2'd0;
+    else
+      counter <= counter + 1;
+  end
 
 endmodule
 
