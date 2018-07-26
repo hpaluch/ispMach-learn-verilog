4b13e7a04278ffac978b88259e09ecd61788e340
Henryk Paluch <hpaluch@seznam.cz>
2018-07-26 20:04:47 +0200

    Using module for counter

--- bb_top.v
+++ bb_top.v
@@ -22,19 +22,7 @@ module bb_top(nled,nrst);
   defparam I1.TIMER_DIV = "1048576";
   OSCTIMER I1 (.DYNOSCDIS(1'b0), .TIMERRES(1'b0), .OSCOUT(), .TIMEROUT(clk) );
 
-  // 2-bit counter (divider by 4)
-  // = 2'd0;  setting initial value from chapter "Initial Values in Verilog", page 315
-  //   file "reference.pdf", "Synplify Pro for Lattice Reference Manual"
-  reg    [1:0] counter = 2'd0; // Has no effect - some source says that 0 is default (maybe that reason?)
-  assign led[3:2] = counter;
-
-  always @(posedge clk or posedge rst)
-  begin
-    if (rst)
-      counter <= 2'd0;
-    else
-      counter <= counter + 1;
-  end
-
+  // connect our 2-bit counter to LED D3 (divide clk 2x) and D4 (divide clk 4x)
+  bb_counter bbc1 ( .rst(rst), .clk(clk), .dout(led[3:2]));
 endmodule
 
