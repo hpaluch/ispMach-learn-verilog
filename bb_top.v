2e76964bfa06bef55fd0f0b0d2274ece11e4a696
Henryk Paluch <hpaluch@seznam.cz>
2018-07-29 08:53:49 +0200

    Running light using decoder 2to4

--- bb_top.v
+++ bb_top.v
@@ -14,7 +14,6 @@ module bb_top(nled,nrst);
 
   // now we can use positive logic (1=active, 0=inactive)
   assign led[0]   = rst;
-  assign led[7:4] = 4'h0;
 
   wire clk; // 4.7Hz internal clock from OSCTIMER module  
   assign led[1]   = clk; // output 4.7Hz clock on LED D2
@@ -22,7 +21,15 @@ module bb_top(nled,nrst);
   defparam I1.TIMER_DIV = "1048576";
   OSCTIMER I1 (.DYNOSCDIS(1'b0), .TIMERRES(1'b0), .OSCOUT(), .TIMEROUT(clk) );
 
-  // connect our 2-bit counter to LED D3 (divide clk 2x) and D4 (divide clk 4x)
-  bb_counter bbc1 ( .rst(rst), .clk(clk), .dout(led[3:2]));
+  wire [1:0] counter2bit;
+  // connect our 2-bit counter to LEDs D3 (divide clk 2x) and D4 (divide clk 4x)
+  assign led[3:2] = counter2bit;
+
+  // counter - from clk to 2-bit dout
+  bb_counter bbc1 ( .rst(rst), .clk(clk), .dout(counter2bit) );
+
+  // decode 2-bit counter to 4 LEDs - D5 to D8 (only one lights at same time)
+  bb_decoder2to4 bbdec1 ( .addrin(counter2bit), .yout(led[7:4]) );
+
 endmodule
 
